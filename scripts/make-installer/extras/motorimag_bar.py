5f0fe81f978d49769b810a09d7d67e63d2cd462e
Christian Kothe <christian.kothe@gmail.com>
2015-10-17 15:28:37 -0700

    Fixed bugs in motorimagery bar example script.

--- scripts/make-installer/extras/motorimag_bar.py
+++ scripts/make-installer/extras/motorimag_bar.py
@@ -1,20 +1,14 @@
-import time
 import matplotlib.pyplot as plt
-from pylsl import ContinuousResolver, StreamInlet
-width = 0.25
-wait_for = 0.25
+from pylsl import resolve_stream, StreamInlet
+
 print('Resolving a Control stream...')
-resolver = ContinuousResolver(prop='name', value='Control')
-while len(resolver.results()) == 0:
-    resolver = ContinuousResolver(prop='name', value='Control')
-    time.sleep(wait_for)
-info = resolver.results()[0]
-inlet = StreamInlet(info)
+streams = resolve_stream('type', 'Control')
+inlet = StreamInlet(streams[0])
 
 hFigure, ax = plt.subplots()
-plt.plot([0,0],[-1,1],linestyle='--',color='k')
+plt.plot([0, 0], [-1, 1], linestyle='--', color='k')
 plt.hold(True)
-hbar = plt.barh([0.5, .5],[0,0], color='kk',height=0.25, align='center')[::]
+hbar = plt.barh(.5,0, color='kk', height=0.25, align='center')[0]
 plt.xlim(xmin=-1.5, xmax=1.5)
 plt.ylim(ymin=0, ymax=1)
 plt.ion()
@@ -25,14 +19,10 @@ while True:
         if not plt.fignum_exists(hFigure.number):
             break
         sample, timestamp = inlet.pull_sample()
-        if not timestamp:
-            continue
-        values = [0, 1] if sample[0] else [-1, 0]
-        for vi,hi in zip(values, hbar):
-            hi.set_width(vi)
-            hi.axes.xaxis.get_major_locator().refresh()
+        hbar.set_width(sample[0]*2-1)
+        hbar.axes.xaxis.get_major_locator().refresh()
         plt.xlim(xmin=-1.5, xmax=1.5)
         hFigure.canvas.draw()
         hFigure.canvas.flush_events()
     except Exception as e:
-        print(str(e))
+        print(e)
