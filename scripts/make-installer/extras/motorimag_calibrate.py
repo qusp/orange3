98b5250901d6e024277cb621b203eed2d2ac7f7a
Christian Kothe <christian.kothe@gmail.com>
2015-10-31 02:35:55 -0700

    Updated calibration script to be more flexible (multi-class and other kinds of instructions).

--- scripts/make-installer/extras/motorimag_calibrate.py
+++ scripts/make-installer/extras/motorimag_calibrate.py
@@ -5,21 +5,25 @@ import matplotlib.pyplot as plt
 import random
 from pylsl import StreamInfo, StreamOutlet
 
-num_trials = 120
-warmup_trials = 20
+warmup_trials = 10
+trials_per_class = 60
+perform_time = 3.5
+wait_time = 1
 pause_every = 30
+pause_duration = 10
 fontsize = 30
-matplotlib.rcParams.update({'font.size': fontsize})
-
 labels = ['L', 'R']
 markers = ['left', 'right']
+
+matplotlib.rcParams.update({'font.size': fontsize})
+
 
 info = StreamInfo(name='MotorImag-Markers', type='Markers', channel_count=1,
                   nominal_srate=0, channel_format='string',
                   source_id='t8u43t98u')
 outlet = StreamOutlet(info)
 
-print("Press enter to begin.")
+print("Press [Enter] to begin.")
 x = input()
 
 hFigure, ax = plt.subplots()
@@ -32,30 +36,30 @@ plt.ion()
 plt.draw()
 plt.show()
 try:
-    for trial in range(1, warmup_trials+num_trials+1):
+    for trial in range(1, warmup_trials+trials_per_class*len(labels)+1):
         if not plt.fignum_exists(hFigure.number):
             break
-        choice = random.choice([0, 1])
+        choice = random.choice(range(len(labels)))
         t.set_text(labels[choice])
         if trial == warmup_trials:
-            outlet.push_sample(['calibration-begin'])
+            outlet.push_sample(['calib-begin'])
         if trial > warmup_trials:
             outlet.push_sample([markers[choice]])
         hFigure.canvas.draw()
         hFigure.canvas.flush_events()
-        time.sleep(3)
+        time.sleep(perform_time)
         t.set_text('')
         hFigure.canvas.draw()
         hFigure.canvas.flush_events()
-        time.sleep(1)
+        time.sleep(wait_time)
         if trial % pause_every == 0:
             t.set_text('Pause')
             hFigure.canvas.draw()
             hFigure.canvas.flush_events()
-            time.sleep(10)
+            time.sleep(pause_duration)
             t.set_text('')
         hFigure.canvas.draw()
         hFigure.canvas.flush_events()
 except Exception as e:
     print(e)
-outlet.push_sample(['calibration-end'])
+outlet.push_sample(['calib-end'])
